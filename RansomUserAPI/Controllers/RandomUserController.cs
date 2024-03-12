a00d1631205f60916736a466c7e47b8d3def6e00
Gaurav Sharma <gauravium@gmail.com>
2024-03-12 22:18:44 +0530

    Add project files.

---
+++ RansomUserAPI/Controllers/RandomUserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RansomUserAPI.Controllers
+{
+    [Route("api/RandomUser")]
+    public class RandomUserController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get() 
+        {
+            return Ok();
+        }
+
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _randomUserUrl = "https://randomuser.me/api/";
+
+        [HttpGet("GetUser")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetUser()
+        {
+            var response = await _httpClient.GetAsync(_randomUserUrl);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var userData = JsonConvert.DeserializeObject<object>(responseBody);
+            return Ok(userData);
+        }
+    }
+}
